d5ab2a30fa2296c520d26c68eadfb03bdffc443e
JoaoLucas23 <jllgoncalves23@gmail.com>
2023-11-22 21:40:11 -0300

    corrigindo funcoes de correcao do termometro

--- termometro.py
+++ termometro.py
@@ -6,27 +6,27 @@ class Termometro:
     def imprime_temperatura(self):
         return f'{self.temperatura} {self.unidade_medida}'
 
-    def converte_para_celsius(self, temperatura, unidade_medida):
-        if unidade_medida == 'F':
-            return (temperatura - 32) * 5 / 9
-        elif unidade_medida == 'K':
-            return temperatura - 273.15
+    def converte_para_celsius(self):
+        if self.unidade_medida == 'F':
+            return (self.temperatura - 32) * 5 / 9
+        elif self.unidade_medida == 'K':
+            return self.temperatura - 273.15
         else:
-            return temperatura
+            return self.temperatura
         
-    def converte_para_fahrenheit(self, temperatura, unidade_medida):
-        if unidade_medida == 'C':
-            return (temperatura * 9 / 5) + 32
-        elif unidade_medida == 'K':
-            return (temperatura - 273.15) * 9 / 5 + 32
+    def converte_para_fahrenheit(self):
+        if self.unidade_medida == 'C':
+            return (self.temperatura * 9 / 5) + 32
+        elif self.unidade_medida == 'K':
+            return (self.temperatura - 273.15) * 9 / 5 + 32
         else:
-            return temperatura
+            return self.temperatura
     
-    def converte_para_kelvin(self, temperatura, unidade_medida):
-        if unidade_medida == 'C':
-            return temperatura + 273.15
-        elif unidade_medida == 'F':
-            return (temperatura - 32) * 5 / 9 + 273.15
+    def converte_para_kelvin(self):
+        if self.unidade_medida == 'C':
+            return self.temperatura + 273.15
+        elif self.unidade_medida == 'F':
+            return (self.temperatura - 32) * 5 / 9 + 273.15
         else:
-            return temperatura
+            return self.temperatura
     
